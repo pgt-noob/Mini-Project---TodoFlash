f3a2b274ecc270435aeb32246d5eb1ef8e90cca8
PGT <phangiathuan159@gmail.com>
2022-06-15 19:05:05 +0700

    add update function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,21 @@
 const startBtn = document.querySelector('.create-new-session');
 const form = document.querySelector('.adding-area');
+const submitBtn = document.querySelector('.submit-btn')
+const autoTemplate = `<div class='title flex-row'>
+<div class='term-title double-col'>
+    <Span>Term</Span>
+</div>
+
+<div class='mean-title mean double-col'>
+    <Span>Meaning</Span>
+</div>
+</div>`
 let arr = localStorage.length ? JSON.parse(localStorage.getItem('words')) : [ ];
+let update = false;
 function renderTerm () {
     if (arr.length) {
         arr.forEach(item => {
             addTermElement(item);
-            console.log(item)
         })
     } 
 };
@@ -19,14 +29,32 @@ startBtn.addEventListener('click', function (e) {
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
-    const obj ={
+    if (update) {
+        let id;
+        arr.forEach(item => {
+            if (item.term === update){
+                id = arr.indexOf(item);
+            }
+        });
+        arr[id].term = form.elements['term'].value;
+        arr[id].mean = form.elements['mean'].value;
+        localStorage.setItem('words',JSON.stringify([...arr]));
+        update = false;
+        submitBtn.textContent = 'Add';
+        document.querySelector('.listing').innerHTML = autoTemplate;
+        renderTerm();
+        const elementList = document.querySelectorAll('.listing-item');
+        elementList[id].classList.add('updated');
+        setTimeout(function () {
+            elementList[id].classList.remove('updated');
+        },1200)
+    } else {const obj ={
         term : form.elements['term'].value, 
         mean : form.elements['mean'].value
     };
     arr.push(obj);
     addTermElement(obj);
-    console.log(obj)
-    localStorage.setItem('words',JSON.stringify([...arr]))
+    localStorage.setItem('words',JSON.stringify([...arr]))}
     this.reset();
 })
 
@@ -39,8 +67,14 @@ function addTermElement (item) {
     <div class="item-mean mean double-col">
         ${item.mean}
     </div>
-    <div class="remove-item">
-        <ion-icon name="trash-outline"></ion-icon>
+
+    <div class="functions">
+        <div class="update-item">
+            <ion-icon name="ellipsis-horizontal-outline"></ion-icon>
+        </div>
+        <div class="remove-item">
+            <ion-icon name="trash-outline"></ion-icon>
+        </div>
     </div>
     </div>
     `;
@@ -50,20 +84,24 @@ function addTermElement (item) {
 document.body.addEventListener('click', function (e) {
     e.stopPropagation();
     if (e.target.matches('.remove-item')) {
-        let data = e.target.parentElement.dataset.term;
+        let data = e.target.parentElement.parentElement.dataset.term;
         arr = arr.filter(item => {
             return item.term !== data
         })
         localStorage.setItem('words',JSON.stringify([...arr]));
-        document.querySelector('.listing').innerHTML = `<div class='title flex-row'>
-                        <div class='term-title double-col'>
-                            <Span>Term</Span>
-                        </div>
-
-                        <div class='mean-title mean double-col'>
-                            <Span>Meaning</Span>
-                        </div>
-                        </div>`
+        document.querySelector('.listing').innerHTML = autoTemplate
         renderTerm()
+    } else if (e.target.matches('.update-item')) {
+        let data = e.target.parentElement.parentElement.dataset.term;
+        const target = arr.filter(item => {
+            if (item.term === data) {
+                return item
+            }
+        });
+        form.elements['term'].value = target[0].term;
+        form.elements['mean'].value = target[0].mean;
+        update = data;
+        submitBtn.textContent = 'Adjust word'
     }
-}) 
+});
+
